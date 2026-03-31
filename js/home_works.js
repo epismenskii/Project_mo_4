55078fea4ec21d8e006f5458f9f5b3336fa03ebb
Eduard Pismenskii <eduardpismenskii@MacBook-Air-Eduard.local>
2026-03-31 15:31:32 +0600

    2nd homework

--- js/home_works.js
+++ js/home_works.js
@@ -18,17 +18,53 @@ gmailCheckBtn.addEventListener("click", () => {
 const innerBlock = document.querySelector('.child_block')
 const parentBlock = document.querySelector(".parent_block")
 
-let position = 0;
+let left = 0; topY = 0; 
+
 const move = () => {
-    const parentWidth = parentBlock.clientWidth;
-    const childWidth = innerBlock.clientWidth;
+    const moveX = parentBlock.clientWidth - innerBlock.clientWidth;
+    const moveY = parentBlock.clientHeight - innerBlock.clientHeight;
 
-    if (position <= parentWidth - childWidth) {
-        innerBlock.style.left = `${position}px`;
-        position++;
-
-        requestAnimationFrame(move)
+    if (left < moveX && topY == 0) {
+        innerBlock.style.left = `${left}px`;
+        left++;
+    }else if (left >= moveX && topY <= moveY){
+        innerBlock.style.top = `${topY}px`;
+        topY++;
+    }else if (topY >= moveY && left > 0){
+        innerBlock.style.left = `${left}px`;
+        left--;
+    }else if (left <= moveX && topY > 0){
+        innerBlock.style.top = `${topY}px`;
+        topY--;
     }
+    requestAnimationFrame(move)
 }
 
 move()
+
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetBtn = document.querySelector("#reset");
+const timeElement = document.querySelector("#seconds");
+
+let seconds = 0;
+let interval = null;
+
+startBtn.addEventListener('click', () => {
+   if(interval !== null) return;
+   interval = setInterval(() => {
+    seconds++
+    timeElement.textContent = seconds
+   }, 1000)
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(interval)
+    interval = null;
+})
+resetBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    interval = null;
+    seconds = 0;
+    timeElement.textContent = 0;
+})
