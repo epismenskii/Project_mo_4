d340af184b5903b6eb3ca299390c0573636372cb
Eduard Pismenskii <epismenskii@gmail.com>
2026-04-13 13:45:24 +0600

    5th homework

--- js/lesson.js
+++ js/lesson.js
@@ -33,3 +33,53 @@ const slider = () => {
 }
 
 slider()
+
+// lesson/dz 5
+
+const somInput = document.querySelector("#som")
+const usdInput = document.querySelector("#usd")
+const euroInput = document.querySelector("#eur")
+const error = document.querySelector("#error")
+
+const converter = (element, usdElement, eurElement) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send();
+        request.onload = () => {
+            if(request.status >= 200 && request.status < 300){
+                const data = JSON.parse(request.response);
+                const usdValue = data.usd;
+                const eurValue = data.euro;
+
+                if(element.value === ''){
+                usdElement.value = '';
+                eurElement.value = '';
+                return;
+                }
+                if(element.id === 'som'){
+                    usdElement.value = (element.value / usdValue).toFixed(2);
+                    eurElement.value = (element.value / eurValue).toFixed(2);
+                }else if(element.id === 'usd'){
+                    usdElement.value = (element.value * usdValue).toFixed(2);
+                    eurElement.value = ((element.value / eurValue) * usdValue).toFixed(2)
+                }else if(element.id === 'eur'){
+                    usdElement.value = (element.value * eurValue).toFixed(2)
+                    eurElement.value = ((element.value * eurValue) / usdValue).toFixed(2)
+                }
+            }
+                else{
+                error.style.color = "red";
+                error.style.fontSize = "24px";
+                error.innerHTML = "Прооизошла ошибка на сервере";
+                return;
+            }
+
+        }
+    } 
+}
+
+converter(somInput, usdInput, euroInput)
+converter(usdInput, somInput, euroInput)
+converter(euroInput, somInput, usdInput)
